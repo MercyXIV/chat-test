99bb8c8bc53d30267fe1210db6f89feaf512afec
MercyXIV <75248542+MercyXIV@users.noreply.github.com>
2026-01-09 22:31:04 +0530

    Delete api directory

--- api/debug-read.js
+++
@@ -1,7 +0,0 @@
-import { Redis } from "@upstash/redis"
-
-export default async function handler(req, res) {
-  const redis = Redis.fromEnv()
-  const raw = await redis.lrange("global-chat-history", 0, -1)
-  res.json({ raw })
-}
